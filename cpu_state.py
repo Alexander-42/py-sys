2cd75d334d6d9cb2ab121b8c1cf18daef329573b
Alexander Oiling <alexander.oiling@gmail.com>
2026-09-24 12:44:59 +0900

    Added functionality to observe a single core's tasks

--- cpu_state.py
+++ cpu_state.py
@@ -30,57 +30,111 @@ class CPUState:
         state_vals = state_string.split()[1:]
         return {i:k for i,k in zip(self.__init_keys, state_vals) }        
 
-    def update_state(self, state_string):
+    def update_core_state(self, state_string):
         self.__prev_state_dict = self.__curr_state_dict
         self.__prev_busy = self.__curr_busy
         self.__prev_idle = self.__curr_idle
         self.__curr_state_dict = self.__parse_state_string(state_string)
         self.__curr_idle = int(self.__curr_state_dict["idle"]) + int(self.__curr_state_dict["iowait"])
         self.__curr_busy = sum([int(val) for val in self.__curr_state_dict.values()]) - self.__curr_idle
+        self.calculate_delta()
 
     def calculate_delta(self):
         for key in self.__curr_state_dict.keys():
             self.__delta[f"{key}_del"] = int(self.__curr_state_dict[key]) - int(self.__prev_state_dict[key])
 
-    def calculate_usage(self):
+    def calculate_core_usage(self):
         del_busy = self.__curr_busy-self.__prev_busy
         del_idle = self.__curr_idle-self.__prev_idle
         return 100*del_busy/(del_busy + del_idle)
 
-    def __cpu_usage_bar(self):
-        usage = self.calculate_usage()
-        num_bars = int((usage * BAR_LENGTH) // 100)
+    def calculate_key_usage(self):
+        key_usages=[]
+        for key in self.__delta.keys():
+            key_usages.append(100*self.__delta[key]/(sum(self.__delta.values())))
+        return key_usages
+
+    def print_core_usage(self, terminal_width):
+        usage = self.calculate_core_usage()
+        usage_string = f"{self.name} usage is at {round(usage,1)}%"
+        usage_string += (terminal_width-len(usage_string))*' '
+        print(usage_string)
+
+    def print_key_usages(self, terminal_width):
+        key_usages = self.calculate_key_usage()
+        key_usage_pairs = zip(INIT_KEYS, key_usages)
+        print('% of core usage')
+        for key, usage in key_usage_pairs:
+            usage_string = f"{key}: {round(usage, 1)}%"
+            usage_string += (terminal_width-len(usage_string))*' '
+            print(usage_string)
+
+    def __cpu_usage_bar(self, terminal_width):
+        terminal_width = terminal_width - 15
+        usage = self.calculate_core_usage()
+        num_bars = int((usage * terminal_width) // 100)
         bar = "["
         for _ in range(num_bars):
             bar += "|"
-        for _ in range(num_bars, BAR_LENGTH):
+        for _ in range(num_bars, terminal_width):
             bar += " "
         bar += "]"
 
         return bar, usage
 
-    def print_bar(self):
-        bar, usage = self.__cpu_usage_bar()
-        print(f"{bar} {self.name.strip()} {round(usage, 1)}%")
+    def print_core_bar(self, terminal_width):
+        bar, usage = self.__cpu_usage_bar(terminal_width)
+        bar_string = f"{bar} {self.name.strip()} {round(usage, 1)}%"
+        bar_string += (terminal_width-len(bar_string))*' '
+        print(bar_string)
 
-def init_cpu_states(file):
-    cpus = {}
+def init_core_states(file):
+    cores = {}
 
     for line in file:
-        if line[3] == ' ':
-            cpus[line[0:4]] = CPUState(line, line[0:4])
-        elif line[3].isnumeric():
-            cpus[line[0:4]] = CPUState(line, line[0:4])
+        if line[3].isnumeric():
+            cores[line[0:4]] = CPUState(line, line[0:4])
+        else:
+            pass
 
-    return cpus
+    return cores
 
-def observe_cpu_state(filepath, cpus):
+def init_cpu_state(file):
+    cpu_line = file.readline()
+    cpu = CPUState(cpu_line, "global")
+
+    return cpu
+
+def init_single_core(file, core_no):
+    for line in file:
+        if line[0:4] == f"cpu{core_no}":
+            core = CPUState(line, f"cpu{core_no}")
+
+    return core
+
+def observe_single_core(terminal_width, filepath, core):
     with open(filepath, mode="r") as file:
         for line in file:
-            this_cpu = cpus.get(line[0:4])
-            if this_cpu:
-                this_cpu.update_state(line)
-                this_cpu.print_bar()
-            else:
-                break
+            if line[0:4] == core.name:
+                core.update_core_state(line)
+                core.print_core_usage(terminal_width)
+                core.print_key_usages(terminal_width)
     file.close()
+
+def observe_whole_cpu_usage(terminal_width, filepath, cores, cpu):
+    with open(filepath, mode="r") as file:
+        for line in file:
+            if line[0:4] == "cpu ":
+                cpu.update_core_state(line)
+                cpu.print_core_usage(terminal_width)
+            try:
+                this_core = cores.get(line[0:4])
+                if this_core:
+                    this_core.update_core_state(line)
+                    this_core.print_core_bar(terminal_width)
+            finally:
+                pass
+    file.close()
+
+def print_core_status(core):
+    core.print_bar()
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import cpu_state
 import time
+import shutil
+import argparse
+import os
 
 PROC_STAT_PATH = "/proc/stat"
 
@@ -7,27 +10,80 @@ def initialize_terminal():
     print('\x1b[?1049h')
     print('\x1b[?25l')
     print('\x1b[H')
+    return shutil.get_terminal_size().columns
 
 def cleanup():
     print('\x1b[?1049h')
     print('\x1b[?25h')
     print('\x1b[H')
 
-def main():
+def check_terminal(prev_width):
+    print('\x1b[H')
+    size = shutil.get_terminal_size()
+    width = size.columns
+    if (prev_width != width):
+        print('\x1b[2J') 
+    return width
+    
+def visualize_all():
     with open(PROC_STAT_PATH, mode="r") as file:
-        cpus = cpu_state.init_cpu_states(file)
+        cpu = cpu_state.init_cpu_state(file)
+        cpus = cpu_state.init_core_states(file)
         time.sleep(0.5)
     file.close()
     try:
-        initialize_terminal()
+        initial_width = initialize_terminal()
         while True:
-            print('\x1b[H')
-            cpu_state.observe_cpu_state(PROC_STAT_PATH, cpus)
+            terminal_width = check_terminal(initial_width)
+            initial_width = terminal_width
+            cpu_state.observe_whole_cpu_usage(terminal_width, PROC_STAT_PATH, cpus, cpu)
             time.sleep(.5)
     except KeyboardInterrupt:
         cleanup()
         print("Process safely exited by user")
 
+def visualize_core(core_no):
+    with open(PROC_STAT_PATH, mode="r") as file:
+        core = cpu_state.init_single_core(file, core_no)
+        time.sleep(0.5)
+    file.close()
+    try:
+        initial_width = initialize_terminal()
+        while True:
+            terminal_width = check_terminal(initial_width)
+            initial_width = terminal_width
+            cpu_state.observe_single_core(terminal_width, PROC_STAT_PATH, core)
+            time.sleep(0.5)
+    except KeyboardInterrupt:
+        cleanup()
+        print("Process safely exited by user")
+
+def arg_parser():
+    parser = argparse.ArgumentParser(description="System monitoring written in python")
+
+    parser.add_argument(
+        "-c", "--core",
+        type=int,
+        default=-1,
+        help="Select the CPU core (default: -1 all cores)."
+    )
+
+    return parser.parse_args()
+
+def is_viable_core(core):
+    cores = os.cpu_count()
+    return core >= 0 and core < cores
+
+def main():
+    args = arg_parser()
+
+    if args.core == -1:
+        visualize_all()
+    elif is_viable_core(args.core):
+        visualize_core(args.core)
+    else:
+        print(f"Invalid core: {args.core} (0-{os.cpu_count() - 1})")
+    
 
 if __name__ == "__main__":
     main()
