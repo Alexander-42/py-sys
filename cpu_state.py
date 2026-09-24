05885a8cd078fa053f68c8915c494136730d9c76
Alexander Oiling <alexander.oiling@gmail.com>
2026-09-24 16:39:50 +0900

    Began work on online offline indicator

--- cpu_state.py
+++ cpu_state.py
@@ -13,9 +13,13 @@ INIT_KEYS=[
 
 BAR_LENGTH = 80
 
+EMPTY_INIT_STRING_VALS = ("0"+" ")*9 + "0"
+
 class CPUState:
     def __init__(self, init_string, name):
         self.name = name
+        self.__online = True
+        self.__online = self.__is_online()
         self.init_string = init_string
         self.__init_keys = INIT_KEYS
         self.__prev_state_dict = {}
@@ -30,7 +34,14 @@ class CPUState:
         state_vals = state_string.split()[1:]
         return {i:k for i,k in zip(self.__init_keys, state_vals) }        
 
+    def __is_online(self):
+        if self.name == "global" or self.name == "cpu0":
+            return True
+        with open(f"/sys/devices/system/cpu/{self.name}/online") as file:
+            return file.readline().strip() == "1"
+
     def update_core_state(self, state_string):
+        self.__online = self.__is_online()
         self.__prev_state_dict = self.__curr_state_dict
         self.__prev_busy = self.__curr_busy
         self.__prev_idle = self.__curr_idle
@@ -40,56 +51,95 @@ class CPUState:
         self.calculate_delta()
 
     def calculate_delta(self):
-        for key in self.__curr_state_dict.keys():
-            self.__delta[f"{key}_del"] = int(self.__curr_state_dict[key]) - int(self.__prev_state_dict[key])
+        if self.__online:
+            for key in self.__curr_state_dict.keys():
+                self.__delta[f"{key}_del"] = int(self.__curr_state_dict[key]) - int(self.__prev_state_dict[key])
 
     def calculate_core_usage(self):
-        del_busy = self.__curr_busy-self.__prev_busy
-        del_idle = self.__curr_idle-self.__prev_idle
-        return 100*del_busy/(del_busy + del_idle)
+        if self.__online:
+            del_busy = self.__curr_busy-self.__prev_busy
+            del_idle = self.__curr_idle-self.__prev_idle
+            return 100*del_busy/(del_busy + del_idle)
 
     def calculate_key_usage(self):
-        key_usages=[]
-        for key in self.__delta.keys():
-            key_usages.append(100*self.__delta[key]/(sum(self.__delta.values())))
-        return key_usages
+        if self.__online:
+            key_usages=[]
+            for key in self.__delta.keys():
+                key_usages.append(100*self.__delta[key]/(sum(self.__delta.values())))
+            return zip(INIT_KEYS, key_usages)
 
     def print_core_usage(self, terminal_width):
-        usage = self.calculate_core_usage()
-        usage_string = f"{self.name} usage is at {round(usage,1)}%"
-        usage_string += (terminal_width-len(usage_string))*' '
-        print(usage_string)
-
-    def print_key_usages(self, terminal_width):
-        key_usages = self.calculate_key_usage()
-        key_usage_pairs = zip(INIT_KEYS, key_usages)
-        print('% of core usage')
-        for key, usage in key_usage_pairs:
-            usage_string = f"{key}: {round(usage, 1)}%"
+        if self.__online:
+            usage = self.calculate_core_usage()
+            usage_string = f"{self.name} usage is at {round(usage,1)}%"
             usage_string += (terminal_width-len(usage_string))*' '
             print(usage_string)
+        else:
+            print(f"{self.name} is offline")
+
+    def print_key_usages(self, terminal_width):
+        if self.__online:
+            key_usage_pairs = self.calculate_key_usage()
+            print(f'Key as percentage of core usage')
+            for key, usage in key_usage_pairs:
+                usage_string = f"{key}: {round(usage, 1)}%"
+                usage_string += (terminal_width-len(usage_string))*' '
+                print(usage_string)
 
     def __cpu_usage_bar(self, terminal_width):
         terminal_width = terminal_width - 15
-        usage = self.calculate_core_usage()
-        num_bars = int((usage * terminal_width) // 100)
-        bar = "["
-        for _ in range(num_bars):
-            bar += "|"
-        for _ in range(num_bars, terminal_width):
-            bar += " "
-        bar += "]"
+        if self.__online:
+            usage = self.calculate_core_usage()
+            num_bars = int((usage * terminal_width) // 100)
+            bar = "["
+            for _ in range(num_bars):
+                bar += "|"
+            for _ in range(num_bars, terminal_width):
+                bar += " "
+            bar += "]"
 
-        return bar, usage
+            return bar, usage
+        else:
+            return f"{"["+" "*(terminal_width-2)+"]"}" , "offline"
+
+    def __key_usage_bars(self, terminal_width):
+        bars = []
+        terminal_width = terminal_width - 20
+        key_usage_pairs = self.calculate_key_usage()
+        for key, usage in key_usage_pairs:
+            num_bars = int((usage * terminal_width) // 100)
+            bar = "["
+            for _ in range(num_bars):
+                bar += "|"
+            for _ in range(num_bars, terminal_width):
+                bar += " "
+            bar += "]"
+            bars.append((bar, usage, key))
+        return bars
+            
+        
 
     def print_core_bar(self, terminal_width):
         bar, usage = self.__cpu_usage_bar(terminal_width)
-        bar_string = f"{bar} {self.name.strip()} {round(usage, 1)}%"
+        if self.__online:
+            bar_string = f"{bar} {self.name.strip()} {round(usage, 1)}%"
+        else:
+            bar_string = f"{bar} {self.name.strip()} {usage}"
         bar_string += (terminal_width-len(bar_string))*' '
         print(bar_string)
 
-def init_core_states(file):
-    cores = {}
+    def print_key_bars(self, terminal_width):
+        if self.__online:
+            bars = self.__key_usage_bars(terminal_width)
+            bar_strings = [ f"{bar[0]} {bar[2].strip()} {round(bar[1],1)}%" for bar in bars ]
+            for bstring in bar_strings:
+                bstring += (terminal_width-len(bstring))*' '
+                print(bstring)
+    
+
+
+def init_core_states(file, num_cores):
+    cores = { f"cpu{i}": CPUState(f"cpu{i}" + " " + EMPTY_INIT_STRING_VALS, f"cpu{i}") for i in range(num_cores) }
 
     for line in file:
         if line[3].isnumeric():
@@ -112,16 +162,19 @@ def init_single_core(file, core_no):
 
     return core
 
-def observe_single_core(terminal_width, filepath, core):
+def observe_single_core(bar_on, terminal_width, filepath, core):
     with open(filepath, mode="r") as file:
         for line in file:
             if line[0:4] == core.name:
                 core.update_core_state(line)
                 core.print_core_usage(terminal_width)
-                core.print_key_usages(terminal_width)
+                if bar_on:
+                    core.print_key_bars(terminal_width)
+                else:
+                    core.print_key_usages(terminal_width)
     file.close()
 
-def observe_whole_cpu_usage(terminal_width, filepath, cores, cpu):
+def observe_whole_cpu_usage(bar_on, terminal_width, filepath, cores, cpu):
     with open(filepath, mode="r") as file:
         for line in file:
             if line[0:4] == "cpu ":
@@ -131,9 +184,12 @@ def observe_whole_cpu_usage(terminal_width, filepath, cores, cpu):
                 this_core = cores.get(line[0:4])
                 if this_core:
                     this_core.update_core_state(line)
-                    this_core.print_core_bar(terminal_width)
+                    if bar_on:
+                        this_core.print_core_bar(terminal_width)
+                    else:
+                        this_core.print_core_usage(terminal_width)
             finally:
-                pass
+                break
     file.close()
 
 def print_core_status(core):
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import argparse
 import os
 
 PROC_STAT_PATH = "/proc/stat"
+CORES = os.cpu_count()
 
 def initialize_terminal():
     print('\x1b[?1049h')
@@ -22,13 +23,14 @@ def check_terminal(prev_width):
     size = shutil.get_terminal_size()
     width = size.columns
     if (prev_width != width):
-        print('\x1b[2J') 
+        print('\x1b[2J')
+        print('\x1b[H')
     return width
     
-def visualize_all():
+def visualize_all(bar_on):
     with open(PROC_STAT_PATH, mode="r") as file:
         cpu = cpu_state.init_cpu_state(file)
-        cpus = cpu_state.init_core_states(file)
+        cpus = cpu_state.init_core_states(file, CORES)
         time.sleep(0.5)
     file.close()
     try:
@@ -36,13 +38,13 @@ def visualize_all():
         while True:
             terminal_width = check_terminal(initial_width)
             initial_width = terminal_width
-            cpu_state.observe_whole_cpu_usage(terminal_width, PROC_STAT_PATH, cpus, cpu)
+            cpu_state.observe_whole_cpu_usage(bar_on, terminal_width, PROC_STAT_PATH, cpus, cpu)
             time.sleep(.5)
     except KeyboardInterrupt:
         cleanup()
         print("Process safely exited by user")
 
-def visualize_core(core_no):
+def visualize_core(core_no, bar_on):
     with open(PROC_STAT_PATH, mode="r") as file:
         core = cpu_state.init_single_core(file, core_no)
         time.sleep(0.5)
@@ -51,8 +53,8 @@ def visualize_core(core_no):
         initial_width = initialize_terminal()
         while True:
             terminal_width = check_terminal(initial_width)
+            cpu_state.observe_single_core(bar_on, terminal_width, PROC_STAT_PATH, core)
             initial_width = terminal_width
-            cpu_state.observe_single_core(terminal_width, PROC_STAT_PATH, core)
             time.sleep(0.5)
     except KeyboardInterrupt:
         cleanup()
@@ -68,22 +70,28 @@ def arg_parser():
         help="Select the CPU core (default: -1 all cores)."
     )
 
+    parser.add_argument(
+        "-b", "--bar",
+        action="store_false",
+        help="Disable bar-graph of cpu core usage."
+    )
+
     return parser.parse_args()
 
 def is_viable_core(core):
-    cores = os.cpu_count()
-    return core >= 0 and core < cores
+    return core >= 0 and core < CORES
 
 def main():
     args = arg_parser()
+    print(args)
 
     if args.core == -1:
-        visualize_all()
+        visualize_all(args.bar)
     elif is_viable_core(args.core):
-        visualize_core(args.core)
+        visualize_core(args.core, args.bar)
     else:
-        print(f"Invalid core: {args.core} (0-{os.cpu_count() - 1})")
-    
+        print(f"Invalid core: {args.core} (0-{CORES - 1})")
+
 
 if __name__ == "__main__":
     main()
